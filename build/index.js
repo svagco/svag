70b9f1c0851119722729735cc207ff93fab8e6be
Anton <anton@adc.sh>
2018-09-08 06:27:31 +0300

    build

--- build/index.js
+++ build/index.js
@@ -1,26 +1,5 @@
-const { debuglog } = require('util');
-
-const LOG = debuglog('@svag/svag')
-
-/**
- * The ultimate collection of SVaG elements.
- * @param {Config} config Options for the program.
- * @param {boolean} config.shouldRun A boolean option.
- */
-               async function svag(config) {
-  const {
-    type,
-  } = config
-  LOG('@svag/svag called with %s', type)
-  return type
-}
-
-/* documentary types/index.xml */
-/**
- * @typedef {Object} Config Options for the program.
- * @prop {boolean} shouldRun A boolean option.
- */
+const $svag_window = require('@svag/window');
 
 
-module.exports = svag
+module.exports.Window = $svag_window
 //# sourceMappingURL=index.js.map
